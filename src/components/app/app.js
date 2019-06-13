c13c6fd5ea6b61ecef4a0077c2e46e155cabf3a5
maksymchak <maksym4ak@gmail.com>
2019-06-13 19:38:05 +0300

    create about page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,15 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import { About } from '../pages';
+
 import './app.css';
 
 const App = () => {
   return(
     <Fragment>    
       <Switch> 
+        <Route path="/" component={About} exact />
       </Switch> 
     </Fragment>
   );
