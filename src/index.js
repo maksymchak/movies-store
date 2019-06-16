10ccafe06e36e1e4317e0368bca956caf7527916
maksymchak <maksym4ak@gmail.com>
2019-06-17 02:00:07 +0300

    error handling

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './components/app';
+import ErrorBoundry from './components/error-boundry';
 import MoviestoreService from './services/moviestore-service';
 import { MoviestoreServiceProvider } from './components/moviestore-service-context';
 
 const moviestoreService = new MoviestoreService();
 
 ReactDOM.render(
-  <MoviestoreServiceProvider value={moviestoreService}>
-    <Router>
-      <App/>
-    </Router>
-  </MoviestoreServiceProvider>,
+  <ErrorBoundry>
+    <MoviestoreServiceProvider value={moviestoreService}>
+      <Router>
+        <App/>
+      </Router>
+    </MoviestoreServiceProvider>
+  </ErrorBoundry>,
   document.getElementById('root')
 );
