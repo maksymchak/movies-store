163776bd551c144f1b60588a3f1f89ac7669788a
maksymchak <maksym4ak@gmail.com>
2019-06-17 02:12:50 +0300

    use hoc for service

---
+++ src/components/hoc/with-moviestore-service.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { MoviestoreServiceConsumer } from '../moviestore-service-context';
+
+const withMoviestoreService = () => (Wrapped) => {
+  return (props) => {
+    return (
+      <MoviestoreServiceConsumer>
+        {
+          (moviestoreService) => {
+            return (<Wrapped {...props} moviestoreService={moviestoreService}/>)
+          }
+        }
+      </MoviestoreServiceConsumer>  
+    );
+  }
+};
+
+export {
+  withMoviestoreService
+};
